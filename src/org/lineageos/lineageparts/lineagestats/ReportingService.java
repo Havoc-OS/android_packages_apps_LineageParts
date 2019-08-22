4024880f73efd32e96f12844485b3faa5f5181d1
Pranav Vashi <neobuddy89@gmail.com>
2019-08-22 22:51:55 +0530

    Add initial Havoc-OS stats support
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- src/org/lineageos/lineageparts/lineagestats/ReportingService.java
+++ src/org/lineageos/lineageparts/lineagestats/ReportingService.java
@@ -27,7 +27,7 @@ import android.os.PersistableBundle;
 import android.util.Log;
 
 public class ReportingService extends IntentService {
-    /* package */ static final String TAG = "LineageStats";
+    static final String TAG = "HavocStats";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
 
     public ReportingService() {
@@ -43,39 +43,45 @@ public class ReportingService extends IntentService {
         String deviceId = Utilities.getUniqueID(context);
         String deviceName = Utilities.getDevice();
         String deviceVersion = Utilities.getModVersion();
+        String deviceBuildDate = Utilities.getBuildDate();
+        String deviceAndroidVersion = Utilities.getAndroidVersion();
+        String deviceTag = Utilities.getTag();
         String deviceCountry = Utilities.getCountryCode(context);
         String deviceCarrier = Utilities.getCarrier(context);
         String deviceCarrierId = Utilities.getCarrierId(context);
 
-        final int lineageOldJobId = AnonymousStats.getLastJobId(context);
-        final int lineageOrgJobId = AnonymousStats.getNextJobId(context);
+        final int havocOldJobId = AnonymousStats.getLastJobId(context);
+        final int havocJobId = AnonymousStats.getNextJobId(context);
 
-        if (DEBUG) Log.d(TAG, "scheduling job id: " + lineageOrgJobId);
+        if (DEBUG) Log.d(TAG, "scheduling job id: " + havocJobId);
 
-        PersistableBundle lineageBundle = new PersistableBundle();
-        lineageBundle.putString(StatsUploadJobService.KEY_DEVICE_NAME, deviceName);
-        lineageBundle.putString(StatsUploadJobService.KEY_UNIQUE_ID, deviceId);
-        lineageBundle.putString(StatsUploadJobService.KEY_VERSION, deviceVersion);
-        lineageBundle.putString(StatsUploadJobService.KEY_COUNTRY, deviceCountry);
-        lineageBundle.putString(StatsUploadJobService.KEY_CARRIER, deviceCarrier);
-        lineageBundle.putString(StatsUploadJobService.KEY_CARRIER_ID, deviceCarrierId);
-        lineageBundle.putLong(StatsUploadJobService.KEY_TIMESTAMP, System.currentTimeMillis());
+        PersistableBundle havocBundle = new PersistableBundle();
+        havocBundle.putString(StatsUploadJobService.KEY_DEVICE_NAME, deviceName);
+        havocBundle.putString(StatsUploadJobService.KEY_UNIQUE_ID, deviceId);
+        havocBundle.putString(StatsUploadJobService.KEY_VERSION, deviceVersion);
+        havocBundle.putString(StatsUploadJobService.KEY_BUILD_DATE, deviceBuildDate);
+        havocBundle.putString(StatsUploadJobService.KEY_ANDROID_VERSION, deviceAndroidVersion);
+        havocBundle.putString(StatsUploadJobService.KEY_TAG, deviceTag);
+        havocBundle.putString(StatsUploadJobService.KEY_COUNTRY, deviceCountry);
+        havocBundle.putString(StatsUploadJobService.KEY_CARRIER, deviceCarrier);
+        havocBundle.putString(StatsUploadJobService.KEY_CARRIER_ID, deviceCarrierId);
+        havocBundle.putLong(StatsUploadJobService.KEY_TIMESTAMP, System.currentTimeMillis());
 
         // set job types
-        lineageBundle.putInt(StatsUploadJobService.KEY_JOB_TYPE,
-                StatsUploadJobService.JOB_TYPE_LINEAGEORG);
+        havocBundle.putInt(StatsUploadJobService.KEY_JOB_TYPE,
+                StatsUploadJobService.JOB_TYPE_HAVOC);
 
-        // schedule lineage stats upload
-        js.schedule(new JobInfo.Builder(lineageOrgJobId, new ComponentName(getPackageName(),
+        // schedule stats upload
+        js.schedule(new JobInfo.Builder(havocJobId, new ComponentName(getPackageName(),
                 StatsUploadJobService.class.getName()))
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
                 .setMinimumLatency(1000)
-                .setExtras(lineageBundle)
+                .setExtras(havocBundle)
                 .setPersisted(true)
                 .build());
 
         // cancel old job in case it didn't run yet
-        js.cancel(lineageOldJobId);
+        js.cancel(havocOldJobId);
 
         // reschedule
         AnonymousStats.updateLastSynced(this);
